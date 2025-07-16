a2f740dabab05c85623d64c3b439ced1e421fd34
AmirBagheri <amiramb10@gmail.com>
2025-07-16 15:54:18 +0330

    Enter key press

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,19 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { addTodo , completeTodo , DeleteTodo} from './features/todos/Todo-Slice';
 import './Styles.css'
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 function Home(){
     const [Task,setTask]=useState('')
     let todo = useSelector(state => state.todo.items)
     let dispatch=useDispatch()
     const input=useRef(null)
+    function handlekeyDown(e){
+        if(e.key === 'Enter'){
+            addTaskFunction()
+        }
+    }
+    let addTaskFunction=()=>{dispatch(addTodo(Task)); input.current.value=''; setTask('');}
     return (
         <>
         <div className='flex flex-col justify-center h-[100vh] w-full bg-green-200'>
             <h1 className="text-green-800 font-black text-center">Home</h1>
             <div className='flex flex-col bg-amber-200 p-2 font-black'>
-            add a task <input ref={input} className='m-2 border-2 border-gray-500 rounded-md' placeholder='add your tasks' onChange={(e)=>setTask(e.target.value)}/>
-            <button className='bg-[rgb(59,255,180)] p-1 rounded-md' onClick={()=>{dispatch(addTodo(Task)); input.current.value=''; setTask('');}}>Add it now!</button>
+            add a task <input onKeyPress={(e)=>{handlekeyDown(e)}} ref={input} className='m-2 border-2 border-gray-500 rounded-md' placeholder='add your tasks' onChange={(e)=>setTask(e.target.value)}/>
+            <button className='bg-[rgb(59,255,180)] p-1 rounded-md' onClick={addTaskFunction}>Add it now!</button>
             </div>
             <div className='flex flex-col w-full bg-blend-hue'>
             {todo.map(res=>
